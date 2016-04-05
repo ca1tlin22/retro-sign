cec3408cdc4a01251e8b052b8d6d1fad0ddfe540
ca1tlin22 <gagn0372@algonquinlive.com>
2016-04-05 10:14:36 -0400

    made button change to orange when pressed, pleasing markbot; toggled class on svg

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
 var $svg = $('svg');
 var $lines = $('.lines');
-var $engage = $('.btn:active');
-var $btn = $('btn');
+var $engage = $('.engage');
+var $btn = $('.btn');
 
 $('svg').on('click', function (e) {
   $('.lines').toggleClass('is-pressed');
-  $('.engage').toggleClass('.btn:active');
+  $('.engage').toggleClass('is-engage');
+  $('svg').toggleClass('is-pressed');
 });
