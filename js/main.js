6214509be52db602f51542e6e5c1c6610008edcb
ca1tlin22 <gagn0372@algonquinlive.com>
2016-04-04 20:16:23 -0400

    finally applied fill to letters, nth children created... timings shifted

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
+var $svg = $('svg');
+var $lines = $('.lines');
+var $engage = $('.engage');
+
 $('svg').on('click', function (e) {
   $('.lines').toggleClass('is-pressed');
+  $('.engage').toggleClass('is-engage');
 });
